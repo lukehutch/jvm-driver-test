f97f32510f5f1b03558d4080c61d7332ca2ef64f
Luke Hutchison <luke.hutch@gmail.com>
2021-10-02 04:51:50 -0600

    Update

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,13 +1,14 @@
 package test;
 
-//import io.github.toolfactory.jvm.DefaultDriver;
-//import io.github.toolfactory.jvm.Driver;
+import io.github.toolfactory.jvm.DefaultDriver;
+import io.github.toolfactory.jvm.Driver;
 import io.github.toolfactory.narcissus.Narcissus;
 
 public class Test {
 
     public static void main(String[] args) {
-        //Driver driver = new DefaultDriver();
+        Driver driver = new DefaultDriver();
+        driver.getConsulter(Class.class);
         
         Narcissus.findClass("java.lang.Class");
     }
